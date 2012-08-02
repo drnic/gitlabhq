efcba5afde75d8c7fb3a23679cfbb14c3a47a439
Dr Nic Williams <drnicwilliams@gmail.com>
2012-08-01 21:44:33 -0700

    Load redis.yml for resque configuration

--- config/initializers/4_resque_queues.rb
+++ config/initializers/4_resque_queues.rb
@@ -1,1 +1,6 @@
+redis_config = File.join(Rails.root, "config", "redis.yml")
+if File.exist?(redis_config)
+  config = YAML.load_file(redis_config)
+  Resque.redis = config['uri'] # redis://HOST:PORT
+end
 Resque.watch_queue(PostReceive.instance_variable_get("@queue"))
